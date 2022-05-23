cb7fcb37a6157401782f84d84184d1f01fded9dd
vamsi <gvamsilatha@outlook.com>
2022-05-22 22:43:42 -0500

    added swagger to api project

--- Amics.Api/Startup.cs
+++ Amics.Api/Startup.cs
@@ -91,8 +91,18 @@ namespace Amics.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Amics 2.0");
+                });
             }
-           
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
 
