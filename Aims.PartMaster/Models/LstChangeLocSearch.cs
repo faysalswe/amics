e66171a50dbb1407206e932d7c8f43a9f66322dd
latha <latha@amics.net>
2022-06-30 21:20:48 +0000

    Merged PR 44: Change Location

---
+++ Aims.PartMaster/Models/LstChangeLocSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aims.Core.Models
+{
+    public class LstChangeLocSearch
+    {
+        public string Project { get; set; }
+        public string Name { get; set; }
+        public string SoMain { get; set; }
+        public int LineNum { get; set; }
+        public string SoLinesId { get; set; }
+        public string Itemnumber { get; set; }
+        public string Description { get; set; }
+        public string ItemType { get; set; }
+        public string Quantity { get; set; }
+        public string ItemsId { get; set; }
+        public string InvType { get; set; }
+        public string User { get; set; }
+        public string Warehouse { get; set; }
+        public string Location { get; set; }
+        public string SerNo { get; set; }
+        public string TagNo { get; set; }
+        public string InvSerialId { get; set; }
+        public string InvBasicId { get; set; }
+
+    }
+}
