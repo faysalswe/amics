e36eda3ea76fd401adce90caaa89ff36ac9e6050
vamsilatha gorlagunta <gvamsilatha@outlook.com>
2022-07-07 18:24:58 -0500

    chnage-serial

--- Amics.web/ClientApp/src/app/pages/components/change-serial/change-serial.component.ts
+++ Amics.web/ClientApp/src/app/pages/components/change-serial/change-serial.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentType } from '../../models/componentType';
 import { pmItemSearchResult, pmSearch } from '../../models/pmsearch';
+import { pmSerial } from '../../models/pmSerial';
+import { PartMasterService } from '../../services/partmaster.service';
 import { PartMasterDataTransService } from '../../services/pmdatatransfer.service';
 import { SearchService } from '../../services/search.service';
 
@@ -11,13 +13,14 @@ import { SearchService } from '../../services/search.service';
 })
 export class ChangeSerialComponent implements OnInit {
   changeSerialSearchInfo: changeSerialInfo = new changeSerialInfo();
+  secUserId = 'E02310D5-227F-4DB8-8B42-C6AE3A3CB60B';
   submitButtonOptions = {
     text: "Search",
     useSubmitBehavior: true,
     type: "default"
 
   };
-  changesserialArray = [];
+  changesserialArray: pmSerial[] = [];
   changeserialOptions = [
     'Ware house',
     'Location',
@@ -29,32 +32,56 @@ export class ChangeSerialComponent implements OnInit {
     'Original year',
     'Current year'
   ];
+
+  rowSelection: boolean = false;
+
   constructor(
-    private pmDataTransService: PartMasterDataTransService,
-    private pmdataTransfer: PartMasterDataTransService,
+    private pmService: PartMasterService,
     private searchService: SearchService) { }
 
   ngOnInit(): void {
   }
 
   handleSubmit(e: any) {
-    debugger
-    // console.log(e);
-    e.preventDefault();
 
+    e.preventDefault();
+    this.rowSelection = false;
     let searchDto = new pmSearch();
+    let ItemsId: string;
     searchDto.itemnumber = this.changeSerialSearchInfo.itemNumber;
-    let result = this.searchService.getItemNumberSearchResults(searchDto);
-    // if (!!e.addedItems[0]) {
-    //   this.selectedItem = e.addedItems[0];
-    //   console.log(this.selectedItem);
-    //   this.selectedItemNumber = this.selectedItem.itemNumber;
-    //   this.pmDataTransService.selectedItemChanged(this.selectedItem, this.componentType);
-    // }
+    this.searchService.getItemNumberSearchResults(searchDto).subscribe((response) => {
+      let result = response;
+      ItemsId = String(result?.find(x => x.itemNumber == this.changeSerialSearchInfo.itemNumber)?.id);
+      this.pmService.getViewSerial(ItemsId, this.secUserId).subscribe((res) => {
+        this.changesserialArray = res;
+
+        if (this.changesserialArray.length > 0) {
+          
+          this.changeSerialSearchInfo.fromSerial = this.changesserialArray[0].serlot;
+          this.changeSerialSearchInfo.toSerial = this.changesserialArray[0].serlot;
+          this.changeSerialSearchInfo.fromTagNo = this.changesserialArray[0].tagcol;
+          this.changeSerialSearchInfo.toTagNo = this.changesserialArray[0].tagcol;
+        }
+
+      });
+    });
+  }
+
+  onRowSelection(e: any) {
+    let selectedRow = e.data;
+    this.rowSelection = true;
+    this.changeSerialSearchInfo.fromSerial = selectedRow?.serlot;
+    this.changeSerialSearchInfo.toSerial = selectedRow?.serlot;
+    this.changeSerialSearchInfo.fromTagNo = selectedRow?.tagcol;
+    this.changeSerialSearchInfo.toTagNo = selectedRow?.tagcol;
   }
 
 }
 
 export class changeSerialInfo {
   itemNumber: string = "";
+  fromSerial: string = "";
+  toSerial: string = "";
+  fromTagNo: string = "";
+  toTagNo: string = "";
 }
