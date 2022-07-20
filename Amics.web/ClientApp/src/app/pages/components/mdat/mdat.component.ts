a8b6e8cc7e962985492c30e57e16ba40a6129ddf
vamsilatha gorlagunta <gvamsilatha@outlook.com>
2022-07-20 16:11:35 -0500

    MDAT labels,ERlabels,pm error correction.

--- Amics.web/ClientApp/src/app/pages/components/mdat/mdat.component.ts
+++ Amics.web/ClientApp/src/app/pages/components/mdat/mdat.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LabelMap } from '../../models/Label';
 import { pmDetails } from '../../models/pmdetails';
+import { TextboxStyle } from '../textbox-style/textbox-style';
 
 @Component({
   selector: 'app-mdat',
@@ -13,13 +15,18 @@ export class MdatComponent implements OnInit {
   warehouseList = [];
   mdatList = [];
   now: Date = new Date();
-  constructor() { }
+  StylingMode: string = TextboxStyle.StylingMode;
+  LabelMode: string = TextboxStyle.LabelMode;
+  labelMap: typeof LabelMap;
+  constructor() {
+    this.labelMap = LabelMap;
+  }
 
   ngOnInit(): void {
   }
 
-  onSelectionChanged($event:any){
-    
+  onSelectionChanged($event: any) {
+
   }
 
 }
