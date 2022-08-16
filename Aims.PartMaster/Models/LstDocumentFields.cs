4012422d13854d6e916c4bdb53726b85f19cbcba
latha <latha@amics.net>
2022-08-16 22:59:12 +0530

    Lst document update

---
+++ Aims.PartMaster/Models/LstDocumentFields.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aims.Core.Models
+{
+    public class LstDocumentFields
+    {
+        public string Id { get; set; }
+        public string docName { get; set; }
+        public string ActionFlag { get; set; }
+        public string LineNumber { get; set; }
+        public string ParentId { get; set; }
+        public string PODoc { get; set; }
+        public string SODoc { get; set; }
+        public string WODoc { get; set; }
+    }
+}
